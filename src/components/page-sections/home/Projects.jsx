c5e75b4b5afe10d25fdc73bd35a31f8518b28b23
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-02-27 22:28:05 +0000

    progress.

--- src/components/page-sections/home/Projects.jsx
+++ src/components/page-sections/home/Projects.jsx
@@ -29,7 +29,7 @@ function Projects() {
                     projects.map((item)=>{
                       return(
                         <a href={item.link} target="_blank">
-                          <section className={`rounded p-5 hover:text-white ease duration-500`}>
+                          <section className={`rounded p-5 hover:text-white ease duration-500 bg-[#151515]`}>
                             <p className={`text-3xl md:text-5xl lg:text-8xl text-bn text-${item.align}`}>{item.name} - {item.type}</p>
                           </section>
                         </a>
