d6a90cc7a2abb45770439b9ff4031c5b3803cca5
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-02-28 19:32:18 +0000

    progress

--- src/components/page-sections/home/Projects.jsx
+++ src/components/page-sections/home/Projects.jsx
@@ -8,6 +8,7 @@ function Projects() {
       name:'JB Real Estate',
       type:'Web App',
       link:'https://jbrealestateandproperties.com',
+      url:'jbrealestateandproperties.com',
       description:`This is a project I designed for a client who runs a real estate company.
       He needed a website where he can put new properties up for display, and a system where 
       potential customers can book to view the properties.`,
@@ -18,6 +19,7 @@ function Projects() {
       name:'ARX Architecture',
       type:'Landing Page',
       link:'https://arxarchitecture.netlify.app',
+      url:'arxarchitecture.netlify.app',
       description:`This is a personal project I took on to learn Vite JS. 
       I've always had a thing for architecture, and so I based this website on that. 
       Initially, the plan was to sell this as a template. But plans do change right?`,
@@ -36,13 +38,13 @@ function Projects() {
                     projects.map((item)=>{
                       return(
                           <section className='rounded p-3 lg:p-5 hover:text-white ease duration-500 bg-[#141414]'>
-                            <p className='pb-2'>{item.name} - {item.type}</p>
+                            <p className='pb-2 text-[#999999]'>{item.name} - {item.type}</p>
                             <hr className='border-[#777777]'/>
                             <p className='pt-2 font-light'>{item.description}</p>
                             <br />
                             <p className='font-light'>{item.tools}</p>
                             <br />
-                            <a href={item.link} target="_blank" className='text-gray-300 font-extralight'>arxarchitecture.netlify.app</a>
+                            <a href={item.link} target="_blank" className='text-[#999999] font-extralight'>www.{item.url}</a>
                           </section>
                       )
                     })
