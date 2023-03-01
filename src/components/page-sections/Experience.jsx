b6c458b7f102bdd1760aeb47c78826194214e6db
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-01 18:40:47 +0000

    last1

--- src/components/page-sections/Experience.jsx
+++ src/components/page-sections/Experience.jsx
@@ -60,7 +60,7 @@ function Experience() {
                             /* Use the `selected` state to conditionally style the selected tab. */
                             <button
                               className={
-                                selected ? 'text-white ease duration-500 outline-none border-none appearance-none' : 'ease duration-500 outline-none border-none appearance-none'
+                                selected ? 'text-white ease duration-200 outline-none border-none appearance-none' : 'ease duration-200 outline-none border-none appearance-none'
                               }
                             >
                               <p>{item.PlaceOfWork}</p>
