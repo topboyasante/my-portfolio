f6d9cd577da4a413333db6f4c96743beca7a4239
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-06-10 10:26:58 +0000

    test.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,12 +31,12 @@ export const metadata: Metadata = {
     siteName: "Nana Kwasi Asante",
     images: [
       {
-        url: "../../public/images/v9.jpeg",
+        url: ".https://static1.squarespace.com/static/58519102725e25cc99d17697/5851a284e3df282a35d6173a/616856007677497cfea41857/1634300947012/225283426_536388927445029_2984025784665570619_n.jpg?format=1500w",
         width: 800,
         height: 600,
       },
       {
-        url: "../../public/images/v9.jpeg",
+        url: ".https://static1.squarespace.com/static/58519102725e25cc99d17697/5851a284e3df282a35d6173a/616856007677497cfea41857/1634300947012/225283426_536388927445029_2984025784665570619_n.jpg?format=1500w",
         width: 1800,
         height: 1600,
         alt: "Nana Kwasi Asante",
